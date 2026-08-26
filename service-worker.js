2fdadd5bc9c328118af368cff9e80d0ace20d6f3
lx-god-001 <2291848807@qq.com>
2026-08-26 12:58:10 +0800

    启用手机端内置发音并升级 PWA 缓存

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME='cike-pwa-v3';
-const APP_SHELL=['/','/index.html','/photo-vocabulary.js','/manifest.webmanifest','/icon-192.png','/icon-512.png','/icon-maskable-512.png','/apple-touch-icon.png'];
+const CACHE_NAME='cike-pwa-v4';
+const APP_SHELL=['/','/index.html','/photo-vocabulary.js','/pronunciation-audio.js','/manifest.webmanifest','/icon-192.png','/icon-512.png','/icon-maskable-512.png','/apple-touch-icon.png'];
 
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_SHELL)).then(()=>self.skipWaiting()));
