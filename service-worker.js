7c85fb98113505248a14560e831484369dadb769
lx-god-001 <2291848807@qq.com>
2026-09-04 15:22:02 +0800

    Add batch vocabulary import

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='cike-pwa-v6';
+const CACHE_NAME='cike-pwa-v7';
 const APP_SHELL=['/','/index.html','/photo-vocabulary.js','/pronunciation-audio.js','/manifest.webmanifest','/icon-192.png','/icon-512.png','/icon-maskable-512.png','/apple-touch-icon.png'];
 
 self.addEventListener('install',event=>{
